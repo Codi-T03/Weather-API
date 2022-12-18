98385de1a893669f4b4d450c13180d70997b1bfc
Sarah Awik <sarah@sarahaweek.sarahawik>
2022-12-18 21:39:56 +0200

    Weather API: Breaking the HTML in App.js into React components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-//import Search from "./components/Search";
+import Search from "./components/Search";
 
 //import SayHi, { SayHello } from "./components/WeatherItem";
 import fakeWeatherData from "./fakeWeatherData.json";
@@ -7,6 +7,7 @@ import fakeWeatherData from "./fakeWeatherData.json";
 import clear from "./img/weather-icons/clear.svg";
 import cloudy from "./img/weather-icons/partlycloudy.svg";
 import "./App.css";
+import CurrentWeather from "./components/CurrentWeather";
 
 class App extends Component {
   constructor(props) {
@@ -23,55 +24,8 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <header className="app__header" >
-            <input type="text" placeholder="Type in a City Name"/>
-            <button className="btn">FIND WEATHER</button>
-        </header>
-        <main className="app__main" >
-        <div className="weather__now">
-        <img src={cloudy} className="img__now" />
-            <p>Overcast clouds</p>
-            <p><span>Temprature</span> 10° to 11°C</p>
-            <p><span>Humidity</span> 78% <span>pressure</span> 1008.48</p>
-        </div>
-        <div className="weather__daily">
-        <div>
-                    <p>03:00 </p>
-                    <img src={cloudy} />
-                    <p>8°C</p>
-              </div>
-              <div>
-                    <p>06:00 </p>
-                    <img src={cloudy}  />
-                    <p>9°C</p>
-              </div>
-              <div>
-                    <p>09:00 </p>
-                    <img src={clear}  />
-                    <p>14°C</p>
-              </div>
-              <div>
-                    <p>12:00 </p>
-                    <img src={clear}  />
-                    <p>17°C</p>
-              </div>
-              <div>
-                    <p>15:00 </p>
-                    <img src={clear} />
-                    <p>18°C</p>
-              </div>
-              <div>
-                    <p>18:00 </p>
-                    <img src={clear}  />
-                    <p>16°C</p>
-              </div>
-              <div>
-                    <p>21:00 </p>
-                    <img src={cloudy}  />
-                    <p>13°C</p>
-              </div>
-        </div>
-        </main>
+       <Search></Search>
+        <CurrentWeather></CurrentWeather>
       </div>
     );
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 import clear from "../img/weather-icons/clear.svg";
-import "./Search.css";
+//import "./Search.css";
+import "../App.js";
+
 class Search extends React.Component {
   state = {
     input: ""
@@ -9,23 +11,13 @@ class Search extends React.Component {
 
   render() {
     return (
-      <div>
-        {this.state.input}
-        <input
-          type="text"
-          id="input-name"
-          onChange={event => {
-            this.setState({ input: event.target.value });
-          }}
-        />
-        <button
-          onClick={event => {
-            this.props.handleInput(this.state.input);
-          }}
-        >
-          Say Hello
-        </button>
-      </div>
+     <header className="app__header" >
+            
+        {/* {this.state.input} */}
+        <input type="text" placeholder="Type in a City Name"/>
+            <button className="btn">FIND WEATHER</button>
+      </header>
+
     );
   }
 }
