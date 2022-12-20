6812ae7d0a5edfa2b0d7d0bc88456eac8fbae984
fatima-katrib <fatimakatrib19@gnail.com>
2022-12-20 12:37:21 +0200

    4th step without responsive

--- src/App.js
+++ src/App.js
@@ -2,31 +2,16 @@ import React, { Component } from "react";
 import Search from "./components/Search";
 
 import SayHi, { SayHello } from "./components/WeatherItem";
-import fakeWeatherData from "./fakeWeatherData.json";
-import storm from "./img/weather-icons/storm.svg";
-// import clear from "./img/weather-icons/clear.svg";
-// import cloudy from "./img/weather-icons/cloudy.svg";
-// import drizzle from "./img/weather-icons/drizzle.svg";
-// import fog from "./img/weather-icons/fog.svg";
-// import mostlycloudy from "./img/weather-icons/mostlycloudy.svg";
-// import partlycloudy from "./img/weather-icons/partlycloudy.svg";
-// import rain from "./img/weather-icons/rain.svg";
-// import snow from "./img/weather-icons/snow.svg";
-// import rain from "./img/weather-icons/rain.svg";
-// import unknown from "./img/weather-icons/unknown.svg";
-
+import fakeWeatherData from "./data/FakeWeatherData-2.json";
 import "./App.css";
+import CurrentWeather from "./components/CurrentWeather";
+import WeatherItem from "./components/WeatherItem";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       city: "",
-      lowerTemperature: "",
-      higherTemperature: "",
-      humidity: "",
-      pressure: "",
-      imgSrc: "",
     };
   }
 
@@ -35,24 +20,23 @@ class App extends Component {
   };
 
   render() {
+    console.log(fakeWeatherData.list[0].weather[0].main.toLowerCase());
+    // console.log(fakeWeatherData.list[0].dt_txt.getTime());
+
     return (
       <div className="app">
         <header className="app__header">
           <Search handleInput={this.handleInputChange} />
         </header>
         <main className="app__main" style={{ height: "100vh" }}>
-          <SayHi
-            width="300px"
-            timeDisplay="none"
-            THPDisplay="flex"
-            lowerTemperature="10"
-            higherTemperature="11"
-            humidity="78"
-            pressure="1008.48"
-            imgSrc={storm}
+          <CurrentWeather
+            temp_min={fakeWeatherData.list[0].main.temp_min}
+            temp_max={fakeWeatherData.list[0].main.temp_max}
+            humidity={fakeWeatherData.list[0].main.humidity}
+            pressure={fakeWeatherData.list[0].main.pressure}
+            img={fakeWeatherData.list[0].weather[0].main.toLowerCase()}
             name={this.state.city}
           />
-          {/* <SayHello color="#010745" name={this.state.city} /> */}
           <div
             style={{
               display: "flex",
@@ -62,48 +46,18 @@ class App extends Component {
               marginTop: "50px",
             }}
           >
-            <SayHi
-              THPDisplay="none"
-              width="6vw"
-              meanTemperature="11"
-              imgSrc={storm}
-            />
-            <SayHi
-              width="6vw"
-              THPDisplay="none"
-              meanTemperature="11"
-              imgSrc={storm}
-            />
-            <SayHi
-              width="6vw"
-              THPDisplay="none"
-              meanTemperature="11"
-              imgSrc={storm}
-            />
-            <SayHi
-              width="6vw"
-              THPDisplay="none"
-              meanTemperature="11"
-              imgSrc={storm}
-            />
-            <SayHi
-              width="6vw"
-              THPDisplay="none"
-              meanTemperature="11"
-              imgSrc={storm}
-            />
-            <SayHi
-              width="6vw"
-              THPDisplay="none"
-              meanTemperature="11"
-              imgSrc={storm}
-            />
-            <SayHi
-              width="6vw"
-              THPDisplay="none"
-              meanTemperature="11"
-              imgSrc={storm}
-            />
+            {fakeWeatherData.list.map((e, i) => {
+              if (i < 7) {
+                return (
+                  <WeatherItem
+                    meanTemperature={e.main.temp}
+                    img={e.weather[0].main.toLocaleLowerCase()}
+                    time={fakeWeatherData.list[i].dt_txt}
+                  />
+                );
+              }
+              console.log(this.props.time);
+            })}
           </div>
         </main>
       </div>
--- src/components/WeatherItem.js
+++ src/components/WeatherItem.js
@@ -1,8 +1,53 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import storm from "../img/weather-icons/storm.svg";
+import clear from "../img/weather-icons/clear.svg";
+import cloudy from "../img/weather-icons/cloudy.svg";
+import drizzle from "../img/weather-icons/drizzle.svg";
+import fog from "../img/weather-icons/fog.svg";
+import mostlycloudy from "../img/weather-icons/mostlycloudy.svg";
+import partlycloudy from "../img/weather-icons/partlycloudy.svg";
+import rain from "../img/weather-icons/rain.svg";
+import snow from "../img/weather-icons/snow.svg";
+import unknown from "../img/weather-icons/unknown.svg";
 
 export default class WeatherItem extends Component {
   render() {
+    const getTime = (e) => {
+      let date = e.split(" ");
+      return date[1].slice(0, 5);
+    };
+    let imgSrc = (img) => {
+      switch (img) {
+        case "clear":
+          return clear;
+        case "clouds":
+          return cloudy;
+        case "drizzle":
+          return drizzle;
+        case "fog":
+          return fog;
+        case "storm":
+          return storm;
+        case "mostlycloudy":
+          return mostlycloudy;
+        case "partlycloudy":
+          return partlycloudy;
+        case "rain":
+          return rain;
+        case "snow":
+          return snow;
+        case "unknown":
+          return unknown;
+          break;
+      }
+    };
+    // function getT(time) {
+    //   let time = this.props.time;
+    //   const moonLanding = new Date(time);
+    //   moonLanding = moonLanding.getTime();
+    // }
+    // console.log(this.props.time);
     return (
       <div
         style={{
@@ -12,59 +57,13 @@ export default class WeatherItem extends Component {
           alignItems: "center",
         }}
       >
-        <p style={{ display: this.props.timeDisplay }}>3:00</p>
+        <p>{getTime(this.props.time)}</p>
         <img
-          src={this.props.imgSrc}
-          alt="storm icon"
-          style={{ width: this.props.width, height: "auto" }}
+          src={imgSrc(this.props.img)}
+          alt={this.props.img}
+          style={{ width: "6vw", height: "auto" }}
         />
-        <p style={{ display: this.props.timeDisplay }}>
-          {this.props.meanTemperature}&#176;C
-        </p>
-        <h2 style={{ color: "white", display: this.props.THPDisplay }}>
-          overcast clouds
-        </h2>
-
-        <div
-          className="temp"
-          style={{
-            display: this.props.THPDisplay,
-            alignItems: "center",
-          }}
-        >
-          <h2>Temperature </h2>
-          <span style={{ fontSize: "1.5em", padding: "20px" }}>
-            {this.props.lowerTemperature}&#176; to{" "}
-            {this.props.higherTemperature}&#176;C
-          </span>
-        </div>
-
-        <div
-          className="temp"
-          style={{
-            display: "flex",
-            alignItems: "center",
-            display: this.props.THPDisplay,
-          }}
-        >
-          <h3>Humidity </h3>
-          <span style={{ fontSize: "1em", padding: "20px" }}>
-            {this.props.humidity}%
-          </span>
-          <h3>Pressure </h3>
-          <span style={{ fontSize: "1em", padding: "20px" }}>
-            {this.props.pressure}
-          </span>
-        </div>
-      </div>
-    );
-  }
-}
-export class SayHello extends Component {
-  render() {
-    return (
-      <div style={{ color: this.props.color, backgroundColor: "yellow" }}>
-        Hello {this.props.city}
+        <p>{Math.round(this.props.meanTemperature - 273.15)}&#176;C</p>
       </div>
     );
   }
